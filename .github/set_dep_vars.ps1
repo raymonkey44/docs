167b9515fd409c3d5108e7ca8e6b692cc466c189
RayMon <RayMon@gmail.com>
2023-08-10 21:58:44 -0700

    r

--- .github/set_dep_vars.ps1
+++ .github/set_dep_vars.ps1
@@ -7,8 +7,9 @@ $cnt=1
 foreach ($dep in $arr) {
 	if ($dep){
 		Write-Host Running "Dep$($cnt)Name=$dep >>  $env:GITHUB_OUTPUT"
-		echo "Dep$($cnt++)Name=$dep" >> $env:GITHUB_OUTPUT
-		cat $env:GITHUB_OUTPUT
+		echo "Dep$($cnt)Name=$dep" >> $env:GITHUB_OUTPUT
+		$cnt++
 		
 	}
 }
+cat $env:GITHUB_OUTPUT
